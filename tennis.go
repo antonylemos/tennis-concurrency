33123128fcc48c86f386abcdfefdd3c3d18ebeac
C1328317 Jonas de Oliveira Freire Filho <C1328317@interno.bb.com.br>
2022-12-22 19:11:35 -0300

    docs: documentation line by line of code

--- tennis.go
+++ tennis.go
@@ -1,32 +1,43 @@
 package main
 
+// Import packages
 import (
 	"fmt"
 	"sync"
 	"tennis-concurrency/utils"
 )
 
+// Create a wait group
 var waitGroup sync.WaitGroup
 
+// Player function
 func player(id int, match chan bool, pointsToWin int, playerPoints int) {
+	// Defer the waitGroup.Done() function
 	defer waitGroup.Done()
 
+	// Infinite loop
 	for {
+		// Wait for the channel to be available
 		finish := <-match
 
+		// If the match is finished, return
 		if !finish {
 			return
 		}
 
+		// Start the player's turn
 		fmt.Println("Jogador ", id, " começou a jogada!")
 
+		// Make a point
 		playerScored := utils.MakePoint()
 
+		// If the player scored, increment the player's points
 		if playerScored {
 			playerPoints++
 
 			fmt.Println("Jogador ", id, " marcou ", playerPoints, " pontos!")
 
+			// If the player has reached the number of points to win, print the winner and return
 			if playerPoints == pointsToWin {
 				fmt.Println("Jogador vencedor: ", id)
 
@@ -34,25 +45,35 @@ func player(id int, match chan bool, pointsToWin int, playerPoints int) {
 
 				return
 			}
-		} else {
+		} 
+		// If the player didn't score, print the player's error
+		else {
 			fmt.Println("Jogador ", id, " errou!")
 		}
 
+		// Start the other player's turn
 		match <- true
 	}
 }
 
+// Main function
 func main() {
+	// Create a channel
 	match := make(chan bool)
 
+	// Number of points to win
 	PointsToWin := 10
 
+	// Add two goroutines to wait group
 	waitGroup.Add(2)
 
+	// Start two players
 	go player(1, match, PointsToWin, 0)
 	go player(2, match, PointsToWin, 0)
 
+	// Start the match
 	match <- true
 
+	// Wait for the match to finish
 	waitGroup.Wait()
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,12 @@
 package utils
 
+// Import packages
 import (
 	"math/rand"
 	"time"
 )
 
+// Generate a random number
 func RandomInt() int {
 	seed := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(seed)
@@ -12,8 +14,10 @@ func RandomInt() int {
 	return random.Int()
 }
 
+// Make a point
 func MakePoint() bool {
 	randomNumber := RandomInt()
 
+	// If the random number is even, make a point
 	return randomNumber%2 == 0
 }
